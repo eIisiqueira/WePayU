bda894f3e6dfbf574598c7eadf0be82759fb7a43
Elis Siqueira <eliseduardast@gmail.com>
2026-09-21 14:23:57 -0300

    feat: adiciona hierarquia de empregados e excecoes de validacao

--- src/br/ufal/ic/p2/wepayu/models/Empregado.java
+++ src/br/ufal/ic/p2/wepayu/models/Empregado.java
@@ -1,18 +1,25 @@
 package br.ufal.ic.p2.wepayu.models;
 
-import br.ufal.ic.p2.wepayu.Exception.EmpregadoNaoExisteException;
+import java.math.BigDecimal;
 
-public class Empregado {
+public abstract class Empregado {
+
+    private String id;
     private String nome;
     private String endereco;
-    private String tipo;
-    private int salario;
+    private BigDecimal salario;
+    private boolean sindicalizado;
 
-    public Empregado(String nome, String endereco, String tipo, int salario) throws EmpregadoNaoExisteException {
+    public Empregado(String id, String nome, String endereco, BigDecimal salario) {
+        this.id = id;
         this.nome = nome;
         this.endereco = endereco;
-        this.tipo = tipo;
         this.salario = salario;
+        this.sindicalizado = false;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getNome() {
@@ -23,12 +30,33 @@ public class Empregado {
         return endereco;
     }
 
-    public String getTipo() {
-        return tipo;
-    }
-
-    public int getSalario() {
+    public BigDecimal getSalario() {
         return salario;
     }
 
+    public boolean isSindicalizado() {
+        return sindicalizado;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public void setSindicalizado(boolean sindicalizado) {
+        this.sindicalizado = sindicalizado;
+    }
+
+    public abstract String getTipo();
+
+    public String getComissao() {
+        throw new UnsupportedOperationException();
+    }
 }
